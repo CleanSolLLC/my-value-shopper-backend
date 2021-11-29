8fbe20f435ecb48c68eaa83a21158b08e16fae8b
mamark90 <maqmark90@gmail.com>
2021-11-28 19:23:17 -0500

    made change to application.rb and cors.rb to remove cookie tracking auth handled by auth_controller. Refactored routes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :username, :email
-  #has_many :categories
+  has_many :categories
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-
-  #post '/login', to: 'auth#create'
-  #post '/api/v1/login', to: 'api/v1/auth#create'
-  #delete '/api/v1/logout', to: 'api/v1/sessions#destroy'
-  #get '/api/v1/sessions/get_current_user', to: 'api/v1/sessions#get_current_user'
-  #get '/api/v1/sessions/logout', to: 'api/v1/sessions#delete'
-
   namespace :api do
     namespace :v1 do 
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
       resources :users, only: [:create] do
-        post '/login', to: 'auth#create'
         resources :categories do
           resources :items
           end
