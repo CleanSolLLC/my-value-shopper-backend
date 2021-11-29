bc2281169f13b47e449d85d8f37d14041f17ffb5
mamark90 <maqmark90@gmail.com>
2021-11-28 19:16:53 -0500

    added back in the has_many categories relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  #has_many :categories
+  has_many :categories
   has_secure_password(attribute = :password, validations: true)
   validates :email, uniqueness: { case_sensitive: false }
 end
