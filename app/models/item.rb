9926edb9410e86c814b94c80f3b347f7587feccc
mamark90 <maqmark90@gmail.com>
2022-03-28 12:33:57 -0400

    added back in update_price method in the Item model which was accidentally deleted

--- app/models/item.rb
+++ app/models/item.rb
@@ -43,4 +43,10 @@ class Item < ApplicationRecord
     item.discount = data["discount"]
     user.items << item
   end
+
+  def self.update_price(item, user, coupon_amt)
+    new_sale_price = (item.app_sale_price - coupon_amt).ceil(2)
+    item.update(app_sale_price: new_sale_price, discount: coupon_amt)
+  end
+  
 end
