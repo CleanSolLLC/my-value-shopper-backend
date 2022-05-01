dce83575e5099a9ad2b31b5296ed891f4aa56d44
mamark90 <maqmark90@gmail.com>
2022-05-01 16:21:08 -0400

    created a root route, welcome controller and pseudo erb welcome page to prepare application for hosting on heroku

---
+++ app/controllers/api/v1/welcome_controller.rb
@@ -0,0 +1,2 @@
+class Api::V1::WelcomeController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1 do 
+    namespace :v1 do
+      root to: 'welcome#index' 
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
       resources :users, only: [:create] 
